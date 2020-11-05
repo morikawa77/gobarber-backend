4c4c9b7ec17c0a8807f43ee0df30a7916f20ba2b
Reginaldo Morikawa <morikawa77@gmail.com>
2020-11-04 23:37:11 -0300

    Injeção de dependências

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,18 +1,22 @@
 import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe'
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import IAppointmentsRepository from '../repositories/iAppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
   provider_id: string;
   date: Date;
 }
 
+@injectable()
 class CreateAppointmentService {
   constructor(
-    private appointmentsRepository: IAppointmentsRepository) { }
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) { }
 
   public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
