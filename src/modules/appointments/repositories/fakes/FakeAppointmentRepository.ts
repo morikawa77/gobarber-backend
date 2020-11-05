d8d4d8af6683f6b0adc4dfa236b8880f1bfdbe20
Reginaldo Morikawa <morikawa77@gmail.com>
2020-11-05 04:29:05 -0300

    Testando criação de usuário

--- src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
@@ -20,7 +20,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
   public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
 
-    Object.assign(appointment, { id: uuid(), date, provider_id });
+    Object.assign(appointment, { id: uuidv4(), date, provider_id });
 
     this.appointments.push(appointment);
 
