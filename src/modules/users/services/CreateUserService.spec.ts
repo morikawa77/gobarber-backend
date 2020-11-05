59c274e9253904583ee76b7d48d701e68cadf893
Reginaldo Morikawa <morikawa77@gmail.com>
2020-11-05 05:17:10 -0300

    Testando autenticação

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,13 +1,16 @@
 import AppError from '@shared/errors/AppError';
 
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
     const createUser = new CreateUserService(
       fakeUsersRepository,
+      fakeHashProvider,
     );
 
     const user = await createUser.execute({
@@ -21,8 +24,10 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
     const createUser = new CreateUserService(
       fakeUsersRepository,
+      fakeHashProvider,
     );
 
     await createUser.execute({
