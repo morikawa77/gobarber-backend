b750b16b43e79ebbcbae8f0b20fa2518ccf654a6
Reginaldo Morikawa <morikawa77@gmail.com>
2020-11-11 15:07:12 -0300

    Cache de agendamentos

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -6,6 +6,7 @@ import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   provider_id: string;
@@ -21,6 +22,9 @@ class CreateAppointmentService {
 
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ provider_id, user_id, date }: IRequest): Promise<Appointment> {
@@ -54,10 +58,12 @@ class CreateAppointmentService {
 
     const dateFormatted = format(appointment.date, "dd/MM/yyyy 'às' HH'h'mm");
 
-    await this.notificationsRepository.create({
-      recipient_id: provider_id,
-      content: `Novo agendamento para o dia ${dateFormatted}`,
-    })
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d',
+      )}`
+    );
 
     return appointment;
   }
